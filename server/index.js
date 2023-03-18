48b91e4d19e6a7c18dc35f7d6a48ad30a87c5399
samimasawa <mr.greanmaker@gmail.com>
2023-03-18 22:51:30 +0700

    'test1'

---
+++ server/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "cardsystem",
+});
+
+app.get("/card", (req, res) => {
+    db.query("SELECT * FROM card", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const team = req.body.team;
+  const country = req.body.country;
+  const position = req.body.position;
+  const quantity = req.body.quantity;
+
+  db.query(
+    "INSERT INTO card (name, team, country, position, quantity) VALUES (?,?,?,?,?)",
+    [name, team, country, position, quantity],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const quantity = req.body.quantity;
+  db.query(
+    "UPDATE card SET quantity = ? WHERE id = ?",
+    [quantity, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM card WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+    console.log("Yey, your server is running on port 3001");
+});
